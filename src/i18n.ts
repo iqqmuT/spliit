e9290687abc6dcaf629b516aef480c6e16c80f3e
Andy Trouvé <andy@strekol.eu>
2024-07-29 10:08:50 +0200

    Added french version and title/description from json messages

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,10 @@
 import { getRequestConfig } from 'next-intl/server'
 import { getUserLocale } from './lib/locale'
 
-export const locales = ['en-US', 'fi'] as const
+export const locales = ['en-US', 'fi', 'fr-FR'] as const
 export type Locale = (typeof locales)[number]
 export type Locales = ReadonlyArray<Locale>
-export const defaultLocale: Locale = 'en-US'
+export const defaultLocale: Locale = 'fr-FR'
 
 export default getRequestConfig(async () => {
   const locale = await getUserLocale()
